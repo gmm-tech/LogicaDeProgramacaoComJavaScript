f43656b8d8bdf5b68222117c3e8ce9f2a5f0fe54
Guilherme Miranda de Morais <g.mm-main@proton.me>
2026-09-15 11:02:04 -0300

    Algoritmo para ler as dimensões de um retângulo (base e altura), calcular escrever a área do retângulo. Via alert e prompt

---
+++ exercicio06/script.js
@@ -0,0 +1,8 @@
+alert("Calcule a área do triângulo retângulo")
+
+altura = prompt("Digite a altura: ")
+largura = prompt("Digite a largura: ")
+
+area = altura * largura
+
+alert("A área do retângulo é: " + area)
